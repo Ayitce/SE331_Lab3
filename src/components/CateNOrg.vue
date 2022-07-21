dc7f5b22d9c5ec7305fa847bafb113f7c45659d2
NatchaPetchsikul <natcha_pet@cmu.ac.th>
2022-07-21 19:06:39 +0700

    4.12

---
+++ src/components/CateNOrg.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="categories-and-organizer">
+    <h3>{{ event.category }}</h3>
+    <h3>{{ event.organizer }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CateNOrg',
+  props: {
+    event: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.categories-and-organizer {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+  text-align: right;
+}
+</style>
